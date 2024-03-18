940ce59b714d5a275668f459e3367fec79f056f2
SurenderReddyErigela <31993690+SurenderReddyErigela@users.noreply.github.com>
2024-03-18 13:29:48 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -37,4 +37,6 @@ def predict():
     return render_template('results.html', prediction=round(prediction[0], 2))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use PORT environment variable if available, otherwise default to 5000.
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
